2842f9b8403834100d3018541c5a942ce1ad5449
Breaking BOb <109020948+trpax@users.noreply.github.com>
2023-04-12 11:19:35 +0200

    Add files via upload

---
+++ Console application for working with strings/4/4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name;
+    cout << "enter your name here -->";
+    getline(cin, name);
+    cout << "your name is -->" << name << endl;
+    cout << "it`s correct? (yes/no)\n";
+
+    string answer;
+    string error;
+
+    cin >> answer;
+
+    if (answer != "yes" && answer != "no") {
+        answer = error;
+        cout << "Invalid Input. Please enter only 'yes' or 'no'!";
+        cin >> answer;
+    }
+    if (answer == "yes") {
+        cout << "Great!\n";
+    }
+    else {
+        cout << "I'm sorry.\n";
+    }
+
+    return 0;
+}
---
+++ kantor.ready/kantor/kantor.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+
+using namespace std;
+
+double kant(double uan) {
+	double cal = 8.7;
+	return uan * cal;
+}
+int main() {
+	double uan;
+	cout << "enter amount pln ";
+	cin >> uan;
+	double uann = kant(uan);
+	cout << uan << " pln = " << uann << " in uan"<< endl;
+
+}
+	
